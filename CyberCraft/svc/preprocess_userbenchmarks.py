e727153e46b4da89cba29513812595271fdd5884
Antony Carrard <antony.carrard@heig-vd.ch>
2023-06-16 02:16:28 +0200

    Updated UserBenchmarks preprocessing, updated files

--- CyberCraft/svc/preprocess_userbenchmarks.py
+++ CyberCraft/svc/preprocess_userbenchmarks.py
@@ -14,67 +14,92 @@ class PreProcess:
         self.ram = pd.read_csv(path + "RAM_UserBenchmarks.csv")
         self.ssd = pd.read_csv(path + "SSD_UserBenchmarks.csv")
         
+        # Put the columns name to lowercase
+        self.cpu.columns = map(str.lower, self.cpu.columns)
+        self.gpu.columns = map(str.lower, self.gpu.columns)
+        self.hdd.columns = map(str.lower, self.hdd.columns)
+        self.ram.columns = map(str.lower, self.ram.columns)
+        self.ssd.columns = map(str.lower, self.ssd.columns)
+        
+    def filter_cpu(self):
+        '''
+        Filter the cpu to only keep the unique models
+        '''
+        self.cpu = self.cpu.drop('part number', axis=1)
+    
     def filter_gpu(self):
         '''
         Filter the gpu to only keep the unique models
         '''
         # Only keep the AMD and NVIDIA brands
-        self.gpu = self.gpu[self.gpu['Brand'].str.contains('AMD|Nvidia')]
+        self.gpu = self.gpu[self.gpu['brand'].str.contains('AMD|Nvidia')]
         
         # Remove row that have a part number
-        self.gpu = self.gpu[self.gpu['Part Number'].isnull()]
-        self.gpu = self.gpu.drop('Part Number', axis=1)
+        self.gpu = self.gpu[self.gpu['part number'].isnull()]
+        self.gpu = self.gpu.drop('part number', axis=1)
         
         # Remove models where we have certains keywords that we don't want (like MOBILE of LAPTOP)
-        self.gpu = self.gpu[~self.gpu["Model"].str.contains('Mobile|Laptop|Quadro|GeForce|Radeon HD|FirePro|iGPU')]
+        self.gpu = self.gpu[~self.gpu["model"].str.contains('Mobile|Laptop|Quadro|GeForce|Radeon HD|FirePro|iGPU')]
         
-        # Only keep models containing recent names
-        # self.gpu = self.gpu[self.gpu['Model'].str.contains(['RX', 'RTX', 'Radeon', 'GTX', 'Titan'])]
-        
-        # Only keep 
+        # process the data once filtered
+        self.gpu['model'] = self.gpu['model'].str.replace('-', ' ')
+        self.gpu['brand'] = self.gpu['brand'].str.upper()
         
     def filter_ram(self):
         '''
         Filter the ram
-        '''
+        '''        
         # Remove the old DDR3 standard
-        self.ram = self.ram[~self.ram['Model'].str.contains('DDR3')]
+        self.ram = self.ram[~self.ram['model'].str.contains('DDR3')]
         
         # Get the number of sticks and the memory of each one
-        self.ram[['Number of sticks', 'Capacity of each stick']] = self.ram['Model'].str.extract(r'(\d+)x(\d+)GB').astype(int)
+        self.ram[['number of sticks', 'memory of each stick']] = self.ram['model'].str.extract(r'(\d+)x(\d+)GB').astype(int)
         
         # Add a column with the total memory
-        self.ram['Total capacity'] = self.ram['Number of sticks'] * self.ram['Capacity of each stick']
+        self.ram['memory'] = self.ram['number of sticks'] * self.ram['memory of each stick']
         
-    def get_memory(self, cell:str, tb_to_gb:int=1000):
-            match = re.search(r'(\d+)\s*(T|G)B', cell)
-            if match:
-                value = int(match.group(1))
-                unit = match.group(2)
-                if unit == 'T':
-                    value *= tb_to_gb
-                return value
-            else:
-                return None
+    def get_memory(self, cell:str, re_memory:str, tb_to_gb:int=1000):
+        match = re.search(re_memory, cell)
+        if match:
+            value = int(match.group(1))
+            unit = match.group(2)
+            if unit == 'T':
+                value *= tb_to_gb
+            return value
+        else:
+            return None
     
     def filter_hdd(self):
         '''
         Filter the hdd
         '''
-        self.hdd['Memory'] = self.hdd['Model'].apply(self.get_memory)
+        # Extract the HDD memory
+        re_memory = r'\s*(\d+)\s*(T|G)B.*'
+        self.hdd['memory'] = self.hdd.apply(lambda row: self.get_memory(row['model'], re_memory=re_memory, tb_to_gb=1024), axis=1)
+        self.hdd['model'] = self.hdd['model'].str.replace(f'(?i){re_memory}', '', regex=True)
 
     def filter_ssd(self):
         '''
         Filter the ssd
-        '''
-        self.ssd['Memory'] = self.ssd.apply(lambda row: self.get_memory(row['Model'], tb_to_gb=1024), axis=1)
+        '''        
+        # Remove other terms from the designation column
+        terms_to_remove = ['SSD', 'M.2', 'PCIe', 'NVMe', '2280', 'NAND', '3D', 'WD']
+        for term in terms_to_remove:
+            self.ssd['model'] = self.ssd['model'].str.replace(f'(?i){term}', '', regex=True)
+        
+        # Extract the SSD memory
+        re_memory = r'\s*(\d+)\s*(T|G)B.*'
+        self.ssd['memory'] = self.ssd.apply(lambda row: self.get_memory(row['model'], re_memory=re_memory, tb_to_gb=1024), axis=1)
+        self.ssd['model'] = self.ssd['model'].str.replace(f'(?i){re_memory}', '', regex=True)
+        
+        self.ssd = self.ssd.drop('part number', axis=1)
         
         
     def remove_unused_columns(self):
         '''
         Remove columns that are not useful for optimization
         '''
-        columns_to_remove = ['Type', 'Rank', 'Samples', 'URL']
+        columns_to_remove = ['type', 'rank', 'samples', 'url']
         self.cpu = self.cpu.drop(columns_to_remove, axis=1)
         self.gpu = self.gpu.drop(columns_to_remove, axis=1)
         self.hdd = self.hdd.drop(columns_to_remove, axis=1)
@@ -97,6 +122,7 @@ class PreProcess:
 def main():
     preProcess = PreProcess()
     preProcess.remove_unused_columns()
+    preProcess.filter_cpu()
     preProcess.filter_gpu()
     preProcess.filter_hdd()
     preProcess.filter_ram()
